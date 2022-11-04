bf7d2b4d9d706d0073a5addd6356fc2153579cb2
Huang Jia Yi <3a932117@gm.student.ncut.edu.tw>
2022-11-04 11:21:35 +0800

    1-3 產生HomeController，串接並修改home路由，使HomeController回傳View

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('home', function () {
+/*Route::get('home', function () {
     return view('home.index');
-});
+});*/
+
+Route::get('home', [HomeController::class, 'index']);
